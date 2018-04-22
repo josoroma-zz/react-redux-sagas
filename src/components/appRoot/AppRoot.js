51e74c3e8cfa9c5b7342681472399dd11f0024bb
pablo.orozco <pablo.orozco@gorillalogic.com>
2018-04-22 12:49:31 -0600

    AppRoot - mapDispatchToProps => bindActionCreators

---
+++ src/components/appRoot/AppRoot.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+import UserDetails from './UserDetails';
+
+import { getUserProfileWatcher, logoutWatcher } from '../../store/actionCreators/session';
+
+class AppRoot extends Component {
+  componentDidMount() {
+    // Thanks to `mapDispatchToProps`.
+    this.props.getUserProfileWatcher();
+  }
+
+  logout = () => {
+    this.props.logoutWatcher();
+  };
+
+  render() {
+    const { session } = this.props;
+
+    return (
+      <div className="wrapper">
+        <div className="login-box">
+          <h1>AppRoot</h1>
+          {session.user ? <UserDetails user={this.props.session.user} /> : <div />}
+          <button className="error" onClick={this.logout}>
+            Logout
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+AppRoot.propTypes = {
+  session: PropTypes.object,
+  getUserProfileWatcher: PropTypes.func,
+  logoutWatcher: PropTypes.func
+};
+
+const mapStateToProps = ({ session }) => ({
+  session
+});
+
+/**
+ * Turns an object whose values are action creators, into an object with the same keys,
+ * but with every action creator wrapped into a dispatch call so they may be invoked directly.
+ *
+ * - https://github.com/reactjs/redux/blob/master/docs/api/bindActionCreators.md
+ */
+const mapDispatchToProps = dispatch => bindActionCreators({ getUserProfileWatcher, logoutWatcher }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppRoot);
