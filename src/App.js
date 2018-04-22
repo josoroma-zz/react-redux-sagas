5c4956d944c591e63c5ac035a05b2fbc1aca31e6
pablo.orozco <pablo.orozco@gorillalogic.com>
2018-04-22 15:56:37 -0600

    Use CssBaseline to kickstart an elegant, consistent, and simple baseline to build upon

--- src/App.js
+++ src/App.js
@@ -5,14 +5,24 @@ import store from './store';
 
 import Router from './App.router';
 
-import './App.css';
+/**
+ * material-ui@next - Styles
+ *
+ * Use `theme.palette.background.default` for standard devices and a white background for print devices.
+ *
+ * - https://material-ui-next.com/style/css-baseline
+ */
+import CssBaseline from 'material-ui/CssBaseline';
 
 class App extends Component {
   render() {
     return (
-      <Provider store={store}>
-        <Router />
-      </Provider>
+      <React.Fragment>
+        <CssBaseline />
+        <Provider store={store}>
+          <Router />
+        </Provider>
+      </React.Fragment>
     );
   }
 }
